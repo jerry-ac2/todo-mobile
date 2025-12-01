17c6b14bd3417f4eaff81de840ce5af4d572f118
Jeremiah <jeremiahegemonye12@gmail.com>
2025-12-01 09:43:27 +0100

    changes made

---
+++ app/index.tsx
@@ -0,0 +1,186 @@
+import { useCallback, useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function Index() {
+  const [todo, setTodo] = useState<string[]>([]);
+  const [text, setText] = useState<string | any>("");
+
+  const saveToStorage = useCallback(async () => {
+    try {
+      await AsyncStorage.setItem("todo", JSON.stringify(todo));
+    } catch (e) {
+      console.log(e);
+    }
+  }, [todo]);
+
+  const loadFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem("todo");
+      if (value !== null) {
+        setTodo(JSON.parse(value));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
+
+  useEffect(() => {
+    saveToStorage();
+  }, [todo, saveToStorage]);
+
+  const handleAddToDo = (el: string) => {
+    if (el.length === 0) {
+      alert("please enter a task");
+    } else {
+      setTodo([...todo, el]);
+      setText("");
+    }
+  };
+
+  const handleDelete = (index: number) => {
+    const newTodo = todo.filter((i) => todo.indexOf(i) !== index);
+    setTodo(newTodo);
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#41393E",
+      }}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>TODO Application</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholderTextColor={"#ffff"}
+            placeholder="Enter your task"
+            style={styles.input}
+            value={text}
+            onChangeText={(e) => setText(e)}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+            onPress={() => handleAddToDo(text)}
+          >
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {todo.map((item, index) => (
+            <TaskList
+              key={index}
+              handleDelete={handleDelete}
+              index={index}
+              item={item}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function TaskList({
+  item,
+  index,
+  handleDelete,
+}: {
+  item: string;
+  index: number;
+  handleDelete: (index: number) => void;
+}) {
+  return (
+    <View style={styles.taskListContainer}>
+      <View style={styles.taskList}>
+        <Text>{item}</Text>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={() => handleDelete(index)}
+        >
+          <Text>delete</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#ffff",
+    fontWeight: "bold",
+    fontSize: 30,
+    letterSpacing: 5,
+  },
+  buttonText: {
+    color: "#101D42",
+    fontWeight: "bold",
+    fontSize: 30,
+    letterSpacing: 5,
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    width: "15%",
+    padding: 5.5,
+    backgroundColor: "#ffff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  input: {
+    padding: 15,
+    borderColor: "#ccc",
+    color: "#ffff",
+    borderWidth: 1,
+    width: "70%",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  container: {
+    width: "100%",
+    flex: 1,
+    gap: 50,
+    alignItems: "center",
+    marginTop: 50,
+  },
+  taskList: {
+    padding: 15,
+    backgroundColor: "#ffff",
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 15,
+    borderRightColor: "#657855ff",
+    borderRightWidth: 10,
+  },
+  taskListContainer: {
+    width: "90%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  deleteButton: {
+    display: "flex",
+    backgroundColor: "#ffff",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
